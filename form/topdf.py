ba0855bd2c28ddef445e1c579814b50ef7318d8a
Andrey I <extym@ya.ru>
2022-08-25 19:35:18 +0300

    work form

--- form/topdf.py
+++ form/topdf.py
@@ -1,14 +1,14 @@
 import pdfkit
 from pdfkit import PDFKit
 
-pdfkit.from_url('http://forma-avansa.tk:8000/form2','out.pdf')
+pdfkit.from_url('http://forma-avansa.tk:8000/form','out.pdf')
 
 import weasyprint
 from weasyprint.css.validation.properties import page
 
 
 
-pdf = weasyprint.HTML('http://forma-avansa.tk:8000/form2').write_pdf('goog.pdf',)
+pdf = weasyprint.HTML('http://forma-avansa.tk:8000/form').write_pdf('goog.pdf',)
 
 #open('google.pdf', 'wb').write(pdf)
 
@@ -16,4 +16,4 @@ pdf = weasyprint.HTML('http://forma-avansa.tk:8000/form2').write_pdf('goog.pdf',
 # all_pages = [p for doc in documents for p in doc.pages]
 # documents[0].copy(all_pages).write_pdf('combined.pdf')
 
-weasyprint.HTML('http://forma-avansa.tk:8000/form2').write_pdf('weasyprint-website.pdf', zoom=1)
+weasyprint.HTML('http://forma-avansa.tk:8000/form').write_pdf('weasyprint-website.pdf', zoom=1)
--- tyres_wheels/main.py
+++ tyres_wheels/main.py
@@ -2,48 +2,99 @@ import requests
 from flask import Flask, request, app
 import json
 address = 'https://b2b.kolrad.ru/json/hannover/'
-num = 0
+num = 1
 page = 'page' + str(num)
 contract = '/contract68/?'
-Token = '$1$5lOhoSsQ$eOxkDECAapvsE6qAkFE8h1'
+Token = '$1$jjJ1M9JP$Bz5Epibcr9.Stn.ZCQCqN0'
 vendor = 0
 
-link = address + page + contract + 'token=' + Token + '&vendor=' + str(vendor)
+link = address + page + contract + 'token=' + Token + '&vendor=' + str(vendor)# + '&category=3'
+category = 0
 # get start page & pages
+print(link)
 resp = requests.get(link)
-txt = requests.get(link).text
+txt = resp.text
 pages = txt[88:92]
-# err_before = txt[850:944]
-err_after = txt[583:1078]#[983:1078]
-# number = resp.json(["pages"])
-# print(number)
+#print(txt[950:974])
+# err_after = txt[583:1078]#[983:1078]
+# # number = resp.json(["pages"])
 
-print(pages)
+before_t = txt[:974] + "}"
+main_data = json.loads(before_t)
+print(main_data['pages'])
+#print(txt[:1187])
 
-print('!!! - ' + err_after)
-# with open("developer.json", "a") as write_file:
-#     json.dump(resp, write_file) # encode dict into JSON
-# print("Done writing JSON data into .json file")
 
-from requests.exceptions import HTTPError
+#get data from response
+after_t = txt[985:-13].split('},\n        {')
+all_data = [json.loads('{' + after_t[i] + '}') for i in range(len(after_t))]
+print(all_data[0]['name'])
+print(len(all_data))
+print('all_data -', type(all_data))
 
+# for i in range(len(after_t)):
+#     data = '{' + after_t[i] + '}'
+#     also_data = json.loads(data)
+# #
+# print('also_data -', type(also_data))
+# print(also_data)
+# print('data -', type(data))
+# print(data)
+
+
+for i in range(int(10)):
+    links = address + 'page'+ str(i) + contract + 'token=' + Token + '&vendor=' + str(vendor)
+    need_data = resp.text[983: -12]
+    curr_data = []
+    curr_data = curr_data.append(need_data)
+    with open("devvv.json", "a") as write_file:
+        json.dump(curr_data, write_file) # encode dict into JSON
+        #print(f'page {i}')
+print(f"Done writing JSON data into .json file on {i + 1} pages")
+
+
+# print('! - ', txt[974:982])
+# print('!! - ', txt[942:954])
+#
+
+#with open('devv.json', 'r', encoding='utf-8') as fh: #открываем файл на чтение
+# data = [json.loads(line) for line in open('devv.json', 'r')]
+
+tweets =[]
+# for line in open('devv.json', 'r'):
+#     tweets.append(json.loads(line))
 try:
-    # response = requests.get('https://httpbin.org/get')
-    resp.raise_for_status()
-    # access JSOn content
-    jsonResponse = resp.json()
-    print("Entire JSON response")
-    print(jsonResponse)
-
-except HTTPError as http_err:
-    print(f'HTTP error occurred: {http_err}')
-except Exception as err:
-    print(f'Other error occurred: {err}')
+    for line in open('devv.json', 'r'):
+        l = line[1:-1].split('""')
+        tweets.append(json.loads(line))
+except ValueError:  # includes simplejson.decoder.JSONDecodeError
+    print(f'Decoding JSON has failed - {line[:500]}')
 
 
-r = resp.encoding
-pagess = resp.raise_for_status()
-# pagess = resp.json()
-print(pagess)
 
-print("111 - " + r)
+#get data to file - work
+# for i in range(int(pages)):
+#     links = address + 'page'+ str(i) + contract + 'token=' + Token + '&vendor=' + str(vendor)
+#     need_data = resp.text[983: -12]
+#     with open("devvv.json", "a") as write_file:
+#         json.dump(need_data, write_file) # encode dict into JSON
+#         #print(f'page {i}')
+# print(f"Done writing JSON data into .json file on {i + 1} pages")
+
+
+# from requests.exceptions import HTTPError
+#
+# try:
+#     # response = requests.get('https://httpbin.org/get')
+#     resp.raise_for_status()
+#
+#     jsonResponse = resp.json()
+#     print("Entire JSON response")
+#     print(jsonResponse)
+#
+# except HTTPError as http_err:
+#     print(f'HTTP error occurred: {http_err}')
+# except Exception as err:
+#     print(f'Other error occurred: {err}')
+
+
