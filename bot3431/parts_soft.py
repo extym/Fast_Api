8afdbb5914b55ad34659f98dbbb9f64fff147896
extym <extym@ya.ru>
2024-05-03 06:56:29 +0300

    add client api ps

--- bot3431/parts_soft.py
+++ bot3431/parts_soft.py
@@ -4,7 +4,7 @@ import os
 from requests.auth import HTTPBasicAuth
 
 import bot_tg
-from cred import admin_ps_login, admin_ps_pass, ps_link
+from cred import admin_ps_login, admin_ps_pass, ps_link, ps_YM_II_api_key
 from base64 import b64encode
 import requests
 from requests import Session
@@ -92,7 +92,6 @@ def get_orders_v2(customer_id, marketplace_id):
     return datas
 
 
-
 async def make_data_for_request_v2(data_file, market):
     count = 0
     proxy = ''
@@ -102,7 +101,6 @@ async def make_data_for_request_v2(data_file, market):
         item_ids = get_orders_v2(market, marketplace_id=str(number))
         proxy += item_ids.strip() + ' '
         count += 1
-
 
     result = await change_status(proxy.strip())
     if result:
@@ -114,6 +112,7 @@ async def make_data_for_request_v2(data_file, market):
 
     print('All_order_items', proxy)
 
+
 # get_orders_v2(710, marketplace_id='9009797416999')
 
 # get_smth('/regions.json')
@@ -123,3 +122,95 @@ async def make_data_for_request_v2(data_file, market):
 #  {"id":8,"name":"Выдано","code":"vydano"}
 
 # post_smth()
+
+
+## FOR VERSION 1 API - CLIENT ONLY!
+basket = '/api/v1/baskets/'
+add_basket = "/api/v1/baskets"
+
+def get_current_client_smth(metod):
+    url = "https://3431.ru" + metod
+    # token_ps = HTTPBasicAuth(admin_ps_login, admin_ps_pass)
+    params = {
+        "api_key": ps_YM_II_api_key
+    }
+    answer = requests.get(url, params=params)
+
+    print(answer.text)
+
+
+def send_current_basket_to_order():
+    url = "https://3431.ru/api/v1/baskets/order"
+    headers = {
+        "api_key": ps_YM_II_api_key}
+    data = {
+        "api_key": ps_YM_II_api_key
+    }
+    answer = requests.post(url, headers=headers, data=data)
+
+    print("FINAL_result {}".format(answer.text))
+
+
+
+def make_basket(qnt=None, external_id=None, propousal=None):
+    url = "https://3431.ru/api/v1/baskets"
+    headers = {
+        "api_key": ps_YM_II_api_key}
+    data = {
+        "api_key": ps_YM_II_api_key,
+        "oem": propousal.get('oem'),
+        "make_name": propousal.get('make_name'),
+        "detail_name": propousal.get('detail_name'),
+        "qnt": str(qnt),
+        "comment": str(external_id),
+        "min_delivery_day": str(propousal.get('min_delivery_day')),
+        "max_delivery_day": str(propousal.get('max_delivery_day')),
+        "api_hash": propousal.get('hash_key')
+        }
+    answer = requests.post(url, headers=headers, data=data)
+    print("!!!!!!!!!!!!!!!!", answer.text)
+
+    return answer.status_code
+
+
+def choice_function(items, full_items, qnt):
+    listing = sorted(items.values())[:5]
+    print(333, listing)
+    result = []
+    for item in full_items:
+        if item["cost"] in listing and item['qnt'] >= qnt:
+            result.append(item)
+
+    return result
+
+
+def create_resp_is_exist(oem=None, brand=None, qnt=0, external_id=None):
+    # for item, quantity in sku.items:
+    params = {
+        "api_key": ps_YM_II_api_key,
+        "oem": oem,
+        "make_name": brand,
+        "without_cross": True
+    }
+    metod = "/backend/price_items/api/v1/search/get_offers_by_oem_and_make_name"
+    url = "https://3431.ru" + metod
+    answer = requests.get(url, params=params)
+    need_data = answer.json()["data"]
+    if len(need_data) > 0:
+        propousal = {i['hash_key']: i['cost'] for i in need_data if
+                     (i["oem"] == oem and i['make_name'] == brand)}
+        list_propousal = choice_function(propousal, need_data, qnt)
+
+        print(*list_propousal[0].items(), sep='\n')
+        # print(len(propousal))
+
+        result_make_basket = make_basket(propousal=list_propousal[0],
+                                         qnt=qnt, external_id=external_id)
+
+
+
+
+send_current_basket_to_order()
+
+# create_resp_is_exist(brand="STELLOX", oem="42140459SX", qnt=2, external_id=451642783)
+# get_current_client_smth(basket)
